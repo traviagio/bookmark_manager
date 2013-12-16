4ed25cb8103a9bf6130683b14cc562319f14421a
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-16 18:29:15 +0000

    added tags to the links

--- lib/link.rb
+++ lib/link.rb
@@ -1,8 +1,11 @@
 class Link
 	include DataMapper::Resource
 
+	has n, :tags, :through => Resource
+
 	property :id, Serial #will be auto incremented for every second
 	property :title, String
 	property :url, String
+	# property :tags, String
 
 end
--- server.rb
+++ server.rb
@@ -5,7 +5,9 @@ env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './app/models/link' # this needs to be done after datamapper is initialised
+require './lib/link'
+require './lib/tag' 
+# this needs to be done after datamapper is initialised
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -21,8 +23,11 @@ end
 
 
 post '/links' do
-	url = params["url"]
-	title = params["title"]
-	Link.create(:url => url, :title => title)
-	redirect to('/')
+  url = params["url"]
+  title = params["title"]
+  tags = params["tags"].split(" ").map{|tag| Tag.first_or_create(:text => tag)}
+  Link.create(:url => url, :title => title, :tags => tags)
+  redirect to('/')
 end
+
+
