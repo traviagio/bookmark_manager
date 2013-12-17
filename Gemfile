37202aad593bfc5500de51ac4f19431f95e18a21
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-17 16:16:03 +0000

    app adds user account

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'shotgun'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'database_cleaner'
+gem 'bcrypt-ruby'
 
 group :development, :test do
 	gem 'rspec'
--- server.rb
+++ server.rb
@@ -7,6 +7,8 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
 require './lib/tag' 
+require './lib/user'
+
 # this needs to be done after datamapper is initialised
 
 # After declaring your models, you should finalise them
@@ -16,30 +18,24 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 
-# get '/' do
-# 	@links = Link.all
-# 	erb :index
-# end
-
-
-# post '/links' do
-#   url = params['url']
-#   title = params['title']
-#   tags = params["tags"].split(" ").map{|tag| Tag.first_or_create(:text => tag)}
-#   Link.create(:url => url, :title => title, :tags => tags)
-#   redirect to('/')
-# end
-
-# get 'tags/:text' do
-# 	tag = Tag.first(:text => params[:text])
-# 	@links = tag ? tag.links : []
-# 	erb :index
-# end
-
+enable :sessions
+set :session_secret, 'super secret'
 
 get '/' do
   @links = Link.all
   erb :index
+end
+
+get '/users/new' do
+ 	erb :"/users/new"
+end
+
+
+post '/users' do
+  user = User.create(:email => params[:email], 
+              :password => params[:password])
+  session[:user_id] = user.id
+  redirect to('/')
 end
 
 post '/links' do
@@ -54,5 +50,13 @@ get '/tags/:text' do
   tag = Tag.first(:text => params[:text])
   @links = tag ? tag.links : []
   erb :index
+end
+
+helpers do
+
+  def current_user    
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
+
 end
 
