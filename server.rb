5383613d28f5e3fc9195e13a4fcfb4d6c3a198d2
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-18 17:05:44 +0000

    sign in with authentication

--- server.rb
+++ server.rb
@@ -49,6 +49,23 @@ get '/tags/:text' do
   erb :index
 end
 
+get '/sessions/new' do
+  erb :"sessions/new"
+end
+
+post '/sessions' do
+  email, password = params[:email], params[:password]
+  user = User.authenticate(email, password)
+  if user
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    flash[:errors] = ["The email or password are incorrect"]
+    erb :"sessions/new"
+  end
+end
+  
+
 helpers do
 
   def current_user    
@@ -56,4 +73,8 @@ helpers do
   end
 
 end
+
+
+
+
 
