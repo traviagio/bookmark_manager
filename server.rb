91cea3db1b136537082672cf1fad6c5f6e0c042c
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-18 12:30:07 +0000

    server.rb refractoring

--- server.rb
+++ server.rb
@@ -5,17 +5,9 @@ require './lib/link'
 require './lib/tag' 
 require './lib/user'
 require 'rack-flash'
-
-env = ENV["RACK_ENV"] || "development"
-# we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
-
-
+require_relative 'data_mapper_setup'
 
 use Rack::Flash
-
 enable :sessions
 set :session_secret, 'super secret'
 
